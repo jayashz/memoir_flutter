ab29af3438c4f97a4753101645021d34eee790b8
jayashz <jayzstha15@gmail.com>
2025-03-06 00:08:10 +0545

    added memory details screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:memoir/models/place.dart';
+import 'package:memoir/screens/places_details.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -20,6 +21,11 @@ class PlacesList extends StatelessWidget {
     return ListView.builder(
         itemCount: places.length,
         itemBuilder: (ctx, index) => ListTile(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (ctx) =>
+                        PlacesDetailsScreen(place: places[index])));
+              },
               title: Text(
                 places[index].title,
                 style: Theme.of(context)
